b231e94a7b1ca35a65f332864b3e2f9908615b27
Dwijay Bane <dwijaybane2@gmail.com>
2019-06-19 05:32:15 +0000

    Rectifying some issues in CMakelist and ball_chaser

--- src/ball_chaser/src/drive_bot.cpp
+++ src/ball_chaser/src/drive_bot.cpp
@@ -23,8 +23,6 @@ bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser:
     // Publish requested velocity and angle
     motor_command_publisher.publish(motor_command);
 
-    ros::Duration(1).sleep();
-
     // Return a response message
     res.msg_feedback = "Linear Velocity set - lin.x: " + std::to_string(req.linear_x) + "  Angular Angle Set - ang.z: " + std::to_string(req.angular_z);
     ROS_INFO_STREAM(res.msg_feedback);
